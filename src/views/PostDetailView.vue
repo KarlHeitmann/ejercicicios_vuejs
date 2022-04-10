e13bc2fe429ba6140c3926fda2ebe5c543bcd0df
Karl Heitmann <khf1988@gmail.com>
2022-04-10 13:58:31 -0400

    agregado PostDetailView y PostsView

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    <textarea v-autofocus/>
+    <p>Display the content of post with ID of {{ $route.params.id }} here!</p>
+    <div>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
+    <p><RouterLink to="/posts">&lt; Back </RouterLink></p>
+  </div>
+</template>
+
+<script setup>
+/*
+  imports
+*/
+  import { vAutofocus } from '@/directives/vAutofocus'
+  import { useRoute } from 'vue-router'
+
+  const route = useRoute() // route equivalente a this.$route
+
+  const showPostId = () => {
+    console.log("showPostId")
+    alert(`The ID of this post is: ${ route.params.id }`)
+  }
+
+</script>
