70f89cb46fac321cdd3a53ad4dbb267a07fa99a8
Karl Heitmann <khf1988@gmail.com>
2022-04-10 14:05:30 -0400

    listo con section8: use router

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -6,6 +6,12 @@
     <div>
       <button @click="showPostId">Show Post ID</button>
     </div>
+    <div>
+      <button @click="goHomeIn3Seconds">Go Home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goToFirstPost">Go to first post</button>
+    </div>
     <p><RouterLink to="/posts">&lt; Back </RouterLink></p>
   </div>
 </template>
@@ -15,13 +21,30 @@
   imports
 */
   import { vAutofocus } from '@/directives/vAutofocus'
-  import { useRoute } from 'vue-router'
+  import { useRoute, useRouter } from 'vue-router'
 
   const route = useRoute() // route equivalente a this.$route
+  const router = useRouter() // route equivalente a this.$route
 
   const showPostId = () => {
     console.log("showPostId")
     alert(`The ID of this post is: ${ route.params.id }`)
+  }
+
+  const goHomeIn3Seconds = () => {
+    setTimeout(() => {
+      // router.push('/')
+      router.push({ name: 'home'})
+    }, 3000);
+  }
+
+  const goToFirstPost = () => {
+    router.push({
+      name: 'postDetail',
+      params: {
+        id: 'id1'
+      }
+    })
   }
 
 </script>
